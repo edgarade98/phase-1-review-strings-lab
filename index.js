23cac32224f4fb361cb42957c7ebed8a52090c5d
Edgar <eddyade98@gmail.com>
2023-10-03 11:30:52 +0300

    Review

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = "Grace Hopper"; {
+
+const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+console.log(welcomeMessage);
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${currentUser}!";
+console.log(excitedWelcomeMessage);
+
+const firstInitial = currentUser.charAt(0);
+const shortGreeting = "Welcome, ${firstInitial}! ";
+console.log(shortGreeting);
+
+}
