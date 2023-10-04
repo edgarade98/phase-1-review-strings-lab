067c994f5f0d144dc8f4e198d677ef1924c1e484
Edgar <eddyade98@gmail.com>
2023-10-04 12:30:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper"; {
 
-const welcomeMessage = "Welcome to Flatbook, ${currentUser}!";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 console.log(welcomeMessage);
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, ${currentUser}!";
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 console.log(excitedWelcomeMessage);
 
-const firstInitial = currentUser.charAt(0);
-const shortGreeting = "Welcome, ${firstInitial}! ";
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 console.log(shortGreeting);
 
 }
